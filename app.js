c38fb21701af9794091c576f93d2dc0356a0c6da
jensjoh01 <johnnyj2439@gmail.com>
2017-07-17 16:30:35 -0700

    completed calculating cookies sold for all locations

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ var pike = {
   }
 };
 
+
 pike.randomNumber();
 pike.sales();
 pike.render();
@@ -95,3 +96,144 @@ airport.randomNumber();
 airport.sales();
 airport.render();
 airport.total();
+
+var center = {
+  minCust: 3,
+  maxCust: 24,
+  avgSale: 1.2,
+  customers: [],
+  hourlySales: [],
+
+  randomNumber: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.customers.push( Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust);
+    }
+  },
+
+  sales: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.hourlySales[i] = Math.floor(this.customers[i] * this.avgSale);
+    }
+  },
+
+  render: function(){
+    var centerUL = document.getElementById('center');
+    for(var i = 0; i < hours.length; i++){
+      var liEl = document.createElement('li');
+
+      liEl.textContent = hours[i] + ': ' + this.hourlySales[i];
+      centerUL.appendChild(liEl);
+    }
+  },
+
+  total: function(){
+    var centerUL = document.getElementById('center');
+    var cookieTotal = 0;
+    for(var i = 0; i < hours.length; i++){
+      cookieTotal += this.hourlySales[i];
+    }
+    var liEl = document.createElement('li');
+    liEl.textContent = 'Total: ' + cookieTotal;
+    centerUL.appendChild(liEl);
+    return cookieTotal;
+  }
+};
+
+center.randomNumber();
+center.sales();
+center.render();
+center.total();
+
+var hill = {
+  minCust: 3,
+  maxCust: 24,
+  avgSale: 1.2,
+  customers: [],
+  hourlySales: [],
+
+  randomNumber: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.customers.push( Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust);
+    }
+  },
+
+  sales: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.hourlySales[i] = Math.floor(this.customers[i] * this.avgSale);
+    }
+  },
+
+  render: function(){
+    var hillUL = document.getElementById('hill');
+    for(var i = 0; i < hours.length; i++){
+      var liEl = document.createElement('li');
+
+      liEl.textContent = hours[i] + ': ' + this.hourlySales[i];
+      hillUL.appendChild(liEl);
+    }
+  },
+
+  total: function(){
+    var hillUL = document.getElementById('hill');
+    var cookieTotal = 0;
+    for(var i = 0; i < hours.length; i++){
+      cookieTotal += this.hourlySales[i];
+    }
+    var liEl = document.createElement('li');
+    liEl.textContent = 'Total: ' + cookieTotal;
+    hillUL.appendChild(liEl);
+    return cookieTotal;
+  }
+};
+
+hill.randomNumber();
+hill.sales();
+hill.render();
+hill.total();
+
+var alki = {
+  minCust: 3,
+  maxCust: 24,
+  avgSale: 1.2,
+  customers: [],
+  hourlySales: [],
+
+  randomNumber: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.customers.push( Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust);
+    }
+  },
+
+  sales: function(){
+    for(var i = 0; i < hours.length; i++){
+      this.hourlySales[i] = Math.floor(this.customers[i] * this.avgSale);
+    }
+  },
+
+  render: function(){
+    var alkiUL = document.getElementById('alki');
+    for(var i = 0; i < hours.length; i++){
+      var liEl = document.createElement('li');
+
+      liEl.textContent = hours[i] + ': ' + this.hourlySales[i];
+      alkiUL.appendChild(liEl);
+    }
+  },
+
+  total: function(){
+    var alkiUL = document.getElementById('alki');
+    var cookieTotal = 0;
+    for(var i = 0; i < hours.length; i++){
+      cookieTotal += this.hourlySales[i];
+    }
+    var liEl = document.createElement('li');
+    liEl.textContent = 'Total: ' + cookieTotal;
+    alkiUL.appendChild(liEl);
+    return cookieTotal;
+  }
+};
+
+alki.randomNumber();
+alki.sales();
+alki.render();
+alki.total();

07ab3b17f2f418a1cd21196fa2e2ab34385ac140
jensjoh01 <johnnyj2439@gmail.com>
2017-07-18 16:57:48 -0700

    The sales info table has been populated

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 
 var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 var salesData = [];
+var salesTable = document.getElementById('salesData');
 
 function CookieStore(name, minCust, maxCust, avgSale){
-  var salesTable = document.getElementById('salesData');
   var customersEachHour = [];
   var cookieSalesEachHour = [];
   var cookieSalesTotal = 0;
@@ -12,11 +12,14 @@ function CookieStore(name, minCust, maxCust, avgSale){
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgSale = avgSale;
+
+  // createRandomNumberOfCustomers method creates a random number between customer mins and max (inclusive) at each store and pushes it into an array
   this.createRandomNumberOfCustomers = function(){
     for(var i = 0; i < hours.length; i++){
       customersEachHour.push( Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
     }
   };
+  // populateCookieSalesEachHour method calculates cookie sales by multiplying the customers for each hour times the stores average cookie sales
   this.populateCookieSalesEachHour = function(){
     this.createRandomNumberOfCustomers();
     for(var i = 0; i < hours.length; i++){
@@ -24,6 +27,8 @@ function CookieStore(name, minCust, maxCust, avgSale){
       cookieSalesTotal += cookieSalesEachHour[i];
     }
   };
+
+  // render method: creates the elements and populates them. Also calls the method that calculates the cookie hours
   this.render = function() {
     this.populateCookieSalesEachHour();
     var trEl = document.createElement('tr');
@@ -43,6 +48,26 @@ function CookieStore(name, minCust, maxCust, avgSale){
   this.render();
   salesData.push(this);
 }
+// make the header with store hours and daily store totals
+var header = function() {
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.textContent = '';
+  trEl.appendChild(thEl);
+  for(var i = 0; i < hours.length; i++){
+    thEl = document.createElement('th');
+    thEl.textContent = hours[i];
+    trEl.appendChild(thEl);
+  }
+  thEl = document.createElement('th');
+  thEl.textContent = 'Daily Totals:';
+  trEl.appendChild(thEl);
+  salesTable.appendChild(trEl);
+};
+// calls the header first so it appears on the top row
+header();
+
+// creating each new store object
 var pike = new CookieStore('1st and Pike', 23, 65, 6.3);
 var airport = new CookieStore('SeaTac Airport', 3, 24, 1.2);
 var center = new CookieStore('Seattle Center', 11, 38, 3.7);
